ba699c1c9d547bb1f1a7db561a433149fa61dcfd
AlboLock <103566140+AlboLock@users.noreply.github.com>
2025-03-01 14:19:34 +0100

    push push

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function Comida(nombre, precio, ingredientes){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.ingredientes = ingredientes;
+}
+
+function anadirComida(comida){
+    switch (comida){
+        case 'burger':
+            let burger = new Comida('burger', 5, ['Pan', 'carne', 'lechuga', 'tomate', 'queso']);
+            break;
+        case 'perritoCaliente':
+            let perrito = new Comida('perrito', 3.50, ['Pan', 'salchicha', 'kétchup', 'mostaza']);
+            break;
+        case 'bocadillo':
+            let bocadillo = new Comida('bocadillo', 4, ['Pan', 'jamón', 'queso', 'tomate']);
+            break;
+    }
+}
+
 function toggleExtras(idExpandable, header) {
     let expandable = document.getElementById(idExpandable);
     header.classList.toggle('extended');
